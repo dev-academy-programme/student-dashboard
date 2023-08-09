94d19abee4de9bd86b4c6c7bba726d276359fd62
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-09 13:07:22 +1200

    setup express configuration

---
+++ src/server.tsx
@@ -0,0 +1,20 @@
+import * as Path from 'node:path/posix'
+import * as URL from 'node:url'
+import express from 'express'
+// import commitRoutes from './routes/commitRoutes.js'
+
+const server = express()
+export default server
+
+const __dirname = Path.dirname(URL.fileURLToPath(import.meta.url))
+
+// setup public folder
+export const publicFolder = Path.join(__dirname, '../public')
+server.use(express.static(publicFolder))
+
+// setup routes
+// server.use('/api/commits', commitRoutes)
+
+server.get('/', (req, res) => {
+  res.send({ message: 'Hello from the server!' })
+})
