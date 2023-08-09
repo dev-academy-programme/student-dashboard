aff142b60607192114adaf98758473f28c54e24b
Ahmad Anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2023-08-09 13:12:16 +1200

    import router

--- src/server.tsx
+++ src/server.tsx
@@ -1,7 +1,7 @@
 import * as Path from 'node:path/posix'
 import * as URL from 'node:url'
 import express from 'express'
-// import commitRoutes from './routes/commitRoutes.js'
+import commitRoutes from './routes/commitRoutes.js'
 
 const server = express()
 export default server
@@ -12,8 +12,10 @@ const __dirname = Path.dirname(URL.fileURLToPath(import.meta.url))
 export const publicFolder = Path.join(__dirname, '../public')
 server.use(express.static(publicFolder))
 
+server.use(express.json())
+
 // setup routes
-// server.use('/api/commits', commitRoutes)
+server.use('/api/commits', commitRoutes)
 
 server.get('/', (req, res) => {
   res.send({ message: 'Hello from the server!' })
